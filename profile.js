67111e9285d4a1630b6fb254a0256c26a89109ea
Hirendra B <hirendrabalaji3@gmail.com>
2024-09-29 12:52:57 +0530

    add

--- profile.js
+++ profile.js
@@ -17,9 +17,9 @@ const icon=document.getElementById("icon");
 function darktheme(){
     document.body.classList.toggle("dark-theme");
     if(document.body.classList.contains("dark-theme")){
-      icon.src="sun.png";
+      icon.src="./assests/sun.png";
     }
     else{
-        icon.src="moon.png";
+        icon.src="./assests/moon.png";
     }
 }
